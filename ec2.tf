43bcd63bed8865667d242ce6dcba2403bbe19bd4
priyankapeelu <priyankapeelu@icloud.com>
2022-05-13 15:39:54 +0530

    terraform variablescd tt

---
+++ ec2.tf
@@ -0,0 +1,28 @@
+resource "aws_spot_instance_request" "spot" {
+  count                  = var.SPOT_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.INSTANCE_TYPE
+  wait_for_fulfillment   = true
+  vpc_security_group_ids = [aws_security_group.allow_app.id]
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  iam_instance_profile   = var.APP_TYPE == "backend" ? data.terraform_remote_state.iam.outputs.INSTANCE_PROFILE_NAME : null
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
+resource "aws_instance" "od" {
+  count                  = var.OD_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.INSTANCE_TYPE
+  vpc_security_group_ids = [aws_security_group.allow_app.id]
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+}
+
+resource "aws_ec2_tag" "name-tag" {
+  count       = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  resource_id = element(local.ALL_INSTANCE_IDS, count.index)
+  key         = "Name"
+  value       = "${var.COMPONENT}-${var.ENV}"
+}
