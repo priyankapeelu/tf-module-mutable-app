6c29d4662e63d065e5da10dfcfd8156852fccdfd
priyankapeelu <priyankapeelu@icloud.com>
2022-05-13 15:59:08 +0530

    terraform variablescd tt

--- data.tf
+++ data.tf
@@ -27,6 +27,6 @@ data "aws_secretsmanager_secret" "secrets" {
   name = "${var.ENV}/roboshop/secrets"
 }
 
-data "aws_secretsmanager_secret_version" "secrets" {
-  secret_id = data.aws_secretsmanager_secret.secrets.id
+data "aws_secretsmanager_secret_version" "secret" {
+  secret_id = data.aws_secretsmanager_secret.secret.id
 }
