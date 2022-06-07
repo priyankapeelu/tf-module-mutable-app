63833764171dfc6fadc5322c1347d51e3808e505
priyankapeelu <priyankapeelu@icloud.com>
2022-06-07 15:24:59 +0530

    terraform variablescd tt

--- tg.tf
+++ tg.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
-  port     = var.APP_PORT
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
   health_check {
