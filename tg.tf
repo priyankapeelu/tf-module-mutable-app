56385a26c9b800999cb64e54f7f32aa5a48e1361
priyankapeelu <priyankapeelu@icloud.com>
2022-06-07 16:08:21 +0530

    terraform variablescd tt

--- tg.tf
+++ tg.tf
@@ -3,15 +3,6 @@ resource "aws_lb_target_group" "app" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 5
-    path                = "/health"
-    timeout             = 4
-    unhealthy_threshold = 2
-
-  }
 }
 
 resource "aws_lb_target_group_attachment" "instance-attach" {
