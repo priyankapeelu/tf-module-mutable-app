e0abd5e09b8a2a60bcfe58d64c6b3b328cb4c117
priyankapeelu <priyankapeelu@icloud.com>
2022-06-16 18:54:49 +0530

    terraform variablescd tt

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,11 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment   = true
   vpc_security_group_ids = [aws_security_group.allow_app.id]
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  iam_instance_profile   = var.APP_TYPE == "backend" ? data.terraform_remote_state.iam.outputs.INSTANCE_PROFILE_NAME : null
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
+    ENV  = var.ENV
   }
 }
 
@@ -24,4 +26,18 @@ resource "aws_ec2_tag" "name-tag" {
   resource_id = element(local.ALL_INSTANCE_IDS, count.index)
   key         = "Name"
   value       = "${var.COMPONENT}-${var.ENV}"
+}
+
+resource "aws_ec2_tag" "env-tag" {
+  count       = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  resource_id = element(local.ALL_INSTANCE_IDS, count.index)
+  key         = "ENV"
+  value       = var.ENV
+}
+
+resource "aws_ec2_tag" "monitor-tag" {
+  count       = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  resource_id = element(local.ALL_INSTANCE_IDS, count.index)
+  key         = "prometheus-monitor"
+  value       = "yes"
 }
